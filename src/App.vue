7c21c6c176615839e61b68790a4b1fe4f145402c
rustamnsh <rustamnsh@code.edu.az>
2019-09-07 05:10:32 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <Weather />
+  </div>
+</template>
+
+
+<script>
+import Weather from './components/people.vue'
+export default {
+  name: 'app',
+  components: {
+    Weather
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  border-spacing: 0;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 10px;
+  /* display: block */
+}
+.ID{
+  width: 40%
+}
+.NAME{
+  width: 20%
+}
+.ATT{
+  width: 20%
+}
+.VIP{
+  width: 20%
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/people.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>My App</h1>
+    <button v-on:click="getData">Data</button>
+    
+    <div v-for="Data in DataList" :key="Data.id" class="data">
+        <br>
+      <div class="stats">
+     
+        <table >
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>VIP</th>
+                <th>ATTENDING</th>
+            </tr>
+            </thead>
+            <tbody>
+            <td class="ID"><span>{{Data.id}}</span></td>
+            <td class="NAME"><span>{{Data.name}}</span></td>
+            <td class="VIP"><span>{{Data.vip}}</span></td>
+            <td class="ATT"><span>{{Data.attending}}</span></td>
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Data",
+  data() {
+    return {
+      DataList: []
+    };
+  },
+  methods: { 
+    getData() {
+      fetch("data.json")
+        .then(response => response.json())
+        .then(data => (this.DataList = data));
+        
+        
+    }
+  }
+};
+
+</script>
